c2c3c6bb82cc0132a7dd4b4566061aae5bad9090
Emanuel Hanner <emanuel.hanner@icloud.com>
2024-08-22 14:26:46 +0200

    The start of Helium
    Began working on the development environment we are going to use.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace Compiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = "helium.he";            
+            string fileContent = File.ReadAllText(fileName);
+
+            for (int i = 0; i < fileContent.Length; i++)
+            {
+                if (fileContent[i] == '\n')
+                {
+                    Console.WriteLine(i);
+                }                
+                Console.WriteLine(fileContent[i]);
+            }
+
+            string displayContent = fileContent.Replace("\n", "\\n");
+
+            Console.WriteLine("File Content:");
+            Console.WriteLine(fileContent);
+            Console.WriteLine(displayContent);
+
+
+        }
+    }
+}
